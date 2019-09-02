d68656a1e57fb6ff2ab2653d96c5f521fccb3b8b
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-09-03 01:05:17 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelเวล
                 </div>
 
                 <div class="title m-b-md">
